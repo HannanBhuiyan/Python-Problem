e892aec46d891c5d190f1fb5182950b8fda4c45e
HannanBhuiyan <hannan.diuh@gmail.com>
2022-06-23 10:33:54 -0700

    problem 07

---
+++ problem04.py
@@ -0,0 +1,7 @@
+import math
+print(-1 + 4 * 6)
+print((35+ 5) % 7)
+print(round(14 + -4 * 6 / 11))
+print(math.floor(2 + 15 / 6 * 1 - 7 % 2))
+
+
---
+++ problem07.py
@@ -0,0 +1,7 @@
+num1 = int(input())
+num2 = int(input())
+print(f"{num1} + {num2} = {num1+num2}")
+print(f"{num1} - {num2} = {num1-num2}")
+print(f"{num1} * {num2} = {num1*num2}")
+print(f"{num1} / {num2} = {round(num1/num2)}")
+print(f"{num1} * {num2} = {num1%num2}")
